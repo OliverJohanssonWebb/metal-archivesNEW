7225291289525ba8ef7a24093676a82d0cf8c4ae
Oliver Johansson <98oliver.johansson@gmail.com>
2026-03-26 16:32:03 +0100

    responsive

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-  const links = document.querySelectorAll(".new-content a");
-  const pages = document.querySelectorAll("#content-new .page");
-
-  links.forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-
-      const tab = this.dataset.tab; 
-
-      pages.forEach(p =>
-        p.classList.toggle("active", p.classList.contains(tab))
-      );
-    });
-  });
 
   
